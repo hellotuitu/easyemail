3ad60a4207796716a7a0192c69e84f1effbfb2a5
tuitu <1965972530@qq.com>
2017-06-18 17:54:28 +0800

    hello

--- lib/easyemail.rb
+++ lib/easyemail.rb
@@ -33,6 +33,7 @@ class Easyemail
       self.smtp_settings smtp
     end
   end
+
   def smtp_settings smtp
     # common settings for smtp, all provided by user.
     begin
@@ -41,7 +42,7 @@ class Easyemail
         port: smtp["port"],
         authentication: smtp["authentication"],
         user_name: smtp['user_name'],
-        password: smtp['user_password'],
+        password: smtp['password'],
         enable_starttls_auto: smtp["ttl"]
       }
       @from = smtp["user_name"]
@@ -72,49 +73,49 @@ class Easyemail
     end
   end
 
-  def smtp_settings_for_163 user_name, user_password
+  def smtp_settings_for_163 user_name, password
     smtp = {
       address: "smtp.163.com",
       port: 25,
       authentication: "login",
       user_name: user_name,
-      password: user_password,
+      password: password,
       enable_starttls_auto: true
     }
     self.smtp_settings smtp
   end
 
-  def smtp_settings_for_hhu user_name, user_password
+  def smtp_settings_for_hhu user_name, password
     smtp = {
       address: "mail.hhu.edu.cn",
       port: 25,
       authentication: "login",
       user_name: user_name,
-      password: user_password,
+      password: password,
       enable_starttls_auto: false
     }
     self.smtp_settings smtp
   end
 
-  def smtp_settings_for_qq user_name, user_password
+  def smtp_settings_for_qq user_name, password
     smtp = {
       address: "smtp.qq.com",
       port: 25,
       authentication: "login",
       user_name: user_name,
-      password: user_password,
+      password: password,
       enable_starttls_auto: false
     }
     self.smtp_settings smtp
   end
 
-  def smtp_settings_for_gmail user_name, user_password
+  def smtp_settings_for_gmail user_name, password
     smtp = {
       address: "smtp.gmail.com",
       port: 587,
       authentication: "plain",
       user_name: user_name,
-      password: user_password,
+      password: password,
       enable_starttls_auto: true
     }
     self.smtp_settings smtp
