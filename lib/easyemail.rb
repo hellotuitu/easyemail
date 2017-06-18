4d4058dde80f1d7f7e495d90793e75176ca422d3
tuitu <1965972530@qq.com>
2017-06-18 16:29:55 +0800

    hello

--- lib/easyemail.rb
+++ lib/easyemail.rb
@@ -3,6 +3,7 @@ require 'active_support'
 require 'action_mailer'
 
 ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.view_paths = File.dirname(__FILE__)
 
