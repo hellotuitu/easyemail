48d37fa91572fa0882f6b452f59de6273db09708
tuitu <1965972530@qq.com>
2017-06-18 15:42:37 +0800

    hello

--- lib/easyemail.rb
+++ lib/easyemail.rb
@@ -1,7 +1,55 @@
 require "easyemail/version"
 
 class Easyemail
-  # Your code goes here...
+  require 'active_support'
+  require 'action_mailer'
+
+  def initialize
+    ActionMailer::Base.raise_delivery_errors = true
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.view_paths = File.dirname(__FILE__)
+  end
+
+  def smtp_settings smtp
+    ActionMailer::Base.smtp_settings = {
+      address: configuration['smtp'],
+      port: 25,
+      authentication: :login,
+      user_name: smtp['user_name'],
+      password: smtp['user_password'],
+      enable_starttls_auto: false
+    }
+  end
+
+  def from= from
+    @from = from
+  end
+
+  def to= to
+    @to = to
+  end
+
+  def subject= subject
+    @subject = subject
+  end
+
+  def email
+    class Mailer < ActionMailer::Base
+      def send_email from, to, subject, title, content
+        @content = content
+        @title = title
+        mail(
+          to: to
+          from: from
+          subject: subject
+        ) do | format |
+          format.html
+        end
+      end
+    end
+
+    Mailer.send_email(@from, @to, @subject, @title, @content)
+  end
   def self.hello
     p "hi"
   end
